d9d673fc3b7777e3fe0a0ee1b7a634a2baa7138d
Swiiish21 <malcolmotieno@gmail.com>
2024-10-25 12:39:08 +0300

    added simple comments to break down the code to be understandable

--- regularExpressionMatching.py
+++ regularExpressionMatching.py
@@ -1,23 +1,21 @@
-def is_match(s: str, p: str) -> bool:
-    # Base case: if the regular expression is empty, return True if the input string is empty, False otherwise
-    if len(p) == 0:
-        return len(s) == 0
+def is_match(s: str, p: str) -> bool: # we define a function called is_match that returns a boolean value, we assign it 2 parametres s(string) and p(string)
+    #s is for the string to be checked and p is for the regular expression to compare it to.
 
-    # Base case: if the regular expression is a single character, return True if the input string is a single character and they match, False otherwise
-    if len(p) == 1:
-        return len(s) == 1 and (p[0] == s[0] or (p[0] == '.' and s[0] != '\0'))
+    if len(p) == 0: # here we deal with the case where the regular expression  and the string are empty,
+        return len(s) == 0 #if they are both empty we return true, otherwise false
 
 
+    if len(p) == 1: # here we deal with the case where the regular expression has only one character
+        return len(s) == 1 and (p[0] == s[0] or (p[0] == '.' and s[0] != '\0')) #if the first character of the regular expression is the same as the first character of the string, we return true, otherwise false
 
-    # Recursive case: if the regular expression contains a '*', return True if the input string matches the substring before the '*' repeated any number of times, False otherwise
-    if p[1] == '*':
+    # here we deal with the case where the regular expression has more than one character
+    if p[1] == '*': #if the second character of the regular expression is a star, we check if the first character of the string matches the regular expression
         return (p[0] == s[0] or (p[0] == '.' and s[0] != '\0')) and is_match(s, p[2:]) or (s != '' and (p[0] == s[0] or (p[0] == '.' and s[0] != '\0')) and is_match(s[1:], p))
 
-    # Recursive case: if the regular expression does not contain a '*', return True if the input string matches the substring before the '*' repeated once, False otherwise
-    return (s != '' and (p[0] == s[0] or (p[0] == '.' and s[0] != '\0')) and is_match(s[1:], p[1:]))
 
-print(is_match("aa", "a"))  # Output: False
-print(is_match("aa", "a*"))  # Output: True
-print(is_match("ab", ".*"))  # Output: True
-print(is_match("ab", "c*a*b"))  # Output: True
-print(is_match("mississippi", "mis*is*p*."))  # Output: False
+    return (s != '' and (p[0] == s[0] or (p[0] == '.' and s[0] != '\0')) and is_match(s[1:], p[1:])) #if the first character of the regular expression is the same as the first character of the string, we return true, otherwise false
+
+print(is_match("aa", "a"))  # Output: False , reason is the expression wants a single character, the string has two characters
+print(is_match("aa", "a*"))  # Output: True , reason the expression has an asterisk, so it doesn't matter how many 'a's are in the string, it will match the condition
+print(is_match("ab", ".*"))  # Output: True , reason is the expression has a dot and then an asterisk, meaning any character(s) are allowed,
+print(is_match("mississippi", "mis*is*p*."))  # Output: False , reason is the input string has too many 'i' and 's', but the expression doesn't allow it to have all these 'i's and 's's, hence it will return false
